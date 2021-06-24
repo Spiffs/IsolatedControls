431ef6573c9b8e5694b3c27e9a2bed01cbfa8ef9
Oscar <oscarbuzzsmith@gmail.com>
2021-06-24 16:47:07 +1000

    Pushing Control prefab

--- Assets/OS_Scripts/UIManager.cs
+++ Assets/OS_Scripts/UIManager.cs
@@ -6,6 +6,7 @@ namespace UILib
 {
     public enum ButtonID
     {
+        def,
         D1,
         D2,
         D3,
@@ -19,17 +20,45 @@ namespace UILib
 
     public class UIManager : MonoBehaviour
     {
+        public GameObject Character;
+        private RemoteControl CharacterComponent;
 
+        public List<GameObject> buttons = new List<GameObject>();
 
         // Start is called before the first frame update
         void Start()
         {
-
+            CharacterComponent = Character.GetComponent<RemoteControl>();
         }
 
         // Update is called once per frame
         void Update()
         {
+            ButtonID CurrentButton;
+            CurrentButton = 0;
+
+            foreach (GameObject eachobject in buttons)
+            {
+                Button cur = eachobject.GetComponent<Button>();
+                if (cur.GetPressed() == true)
+                {
+                    CurrentButton = cur.LocalButtonID;
+                }
+            }
+
+            if (CurrentButton != 0)
+            {
+                switch (CurrentButton)
+                {
+                    case ButtonID.D1:
+                        {
+                            CharacterComponent.Up();
+                            break;
+                        }
+
+
+                }
+            }
 
         }
     }
