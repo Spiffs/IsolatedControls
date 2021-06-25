a4e383b4083f805efba53dfe45d2e8757995f439
Aaron <A.fuser04@gmail.com>
2021-06-25 11:15:19 +1000

    Sounds

---
+++ Assets/AF_Scripts/PlaysoundonCLick.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaysoundonCLick : MonoBehaviour
+{
+    public AudioClip[] _sound;
+    public AudioSource _sndToPlay;
+
+    public void OnObjectClick()
+    {
+        _sndToPlay.clip = _sound[Random.Range(0, _sound.Length)];
+        _sndToPlay.Play();
+    }
+}
